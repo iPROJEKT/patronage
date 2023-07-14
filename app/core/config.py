f239ab92095eba22681fb9bc56666e00a013cc44
Sergey <san-8082@mail.ru>
2023-07-14 16:07:07 +0300

    59 pass

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     app_title: str = 'Кошачий благотворительный фонд'
     description: str = 'Разработка Сергея направленная на тотальное погружение в мир программирования'
-    database_url: str
+    database_url: str = 'sqlite+aiosqlite:///./charity_fund.db'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
