f4aaa3a04650e4b64f80a7225e26d1e1c432e64e
Sergey <san-8082@mail.ru>
2023-07-18 18:56:12 +0300

    klkl

--- app/core/const.py
+++ app/core/const.py
@@ -2,10 +2,10 @@ from datetime import datetime, timedelta
 
 
 FROM_TIME = (
-        datetime.now() + timedelta(minutes=10)
+    datetime.now() + timedelta(minutes=10)
 ).isoformat(timespec='minutes')
 TO_TIME = (
-        datetime.now() + timedelta(hours=1)
+    datetime.now() + timedelta(hours=1)
 ).isoformat(timespec='minutes')
 MIN_LEGTH_PROJEKT = 1
 MAX_LEGTH_PROJEKT = 100
