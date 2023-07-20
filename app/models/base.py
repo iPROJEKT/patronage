c1cda7bc1e02928163231d20f7e5e46a8ab80752
Sergey <san-8082@mail.ru>
2023-07-20 21:52:02 +0300

    Rev 5

--- app/models/base.py
+++ app/models/base.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, Boolean, DateTime, CheckConstraint
+from sqlalchemy import Column, Integer, Boolean, DateTime
 
 from app.core.db import Base
 
